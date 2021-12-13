8912acbad2ef8f90aa6eee560b7458c6f47c92dc
frpakin <fpakin@gmail.com>
2021-12-13 15:53:11 +0100

    Misc opt

--- day13.py
+++ day13.py
@@ -1,88 +1,61 @@
 import numpy as np
 
 def day13_load(fname):
-    coords = []
+    coords = [[], []]
     pliages = []
     with open(fname) as f:        
         l = f.readline()
         while l != '\n':
-            coords.append([int(c) for c in l.strip().split(sep=',')])
+            c = l.strip().split(sep=',')
+            coords[0].append(int(c[0]))
+            coords[1].append(int(c[1]))
             l = f.readline()
         l = f.readline()   # skip new line
         while l != '':
             args = l.strip().split()
             arg=args[-1].split(sep='=')
-            pliages.append([ arg[0], int(arg[1])])
+            pliages.append([ 0 if arg[0]=='x' else 1, int(arg[1])])
             l = f.readline()
 
-    min_x, min_y = 0, 0
-    max_x, max_y = 0, 0
-    for c in coords:
-        min_x = min(min_x, c[0])
-        max_x = max(max_x, c[0])
-        min_y = min(min_y, c[1])
-        max_y = max(max_y, c[1])
-    
-    m = np.zeros((max_x+1, max_y+1), dtype = np.bool)
-    for c in coords:
-        m[c[0], c[1]] = True
+    # min_x, min_y = np.amin(coords, axis=1)
+    max_x, max_y = np.amax(coords, axis=1)
+    m = np.zeros((max_x+1, max_y+1), dtype = bool)
+    m[coords] = True
 
     return m, pliages        
-        
 
-def fold_x(m, p):
-    s = m.shape
-    n = np.zeros((p, s[1]), dtype = np.bool)
-    coords = np.nonzero(m)
+
+def fold(coords, axis=0, p=0):
     for i in range(len(coords[0])):
-        if coords[0][i] > p:
-            coords[0][i] = p - (coords[0][i] - p)
-    n[coords] = True
-    return n
-
-
-def fold_y(m, p):
-    s = m.shape
-    n = np.zeros((s[0], p), dtype = np.bool)
-    coords = np.nonzero(m)
-    for i in range(len(coords[0])):
-        if coords[1][i] > p:
-            coords[1][i] = p - (coords[1][i] - p)
-    n[coords] = True
-    return n
+        if coords[axis][i] > p:
+            coords[axis][i] = p - (coords[axis][i] - p)
+    return coords
 
 
 def part1(M, pliages):
-    m = M
     p = pliages[0]
-    if p[0] == 'x':
-        m = fold_x(m, p[1])
-    elif p[0] == 'y':
-        m = fold_y(m, p[1])
-    else:
-        print("Error !")
+    coords = fold(np.nonzero(M), p[0], p[1])
+    max_x, max_y = np.amax(coords, axis=1)
+    m = np.zeros((max_x+1, max_y+1), dtype = bool)
+    m[coords] = True
     return m
 
 
 def part2(M, pliages):
-    m = M
+    coords = np.nonzero(M)
     for p in pliages:
-        if p[0] == 'x':
-            m = fold_x(m, p[1])
-        elif p[0] == 'y':
-            m = fold_y(m, p[1])
-        else:
-            print("Error !")
+        coords = fold(coords, p[0], p[1])
+    max_x, max_y = np.amax(coords, axis=1)
+    m = np.zeros((max_x+1, max_y+1), dtype = bool)
+    m[coords] = True
     return m
     
 
 def pprint(m):
-    for l in range(m.shape[1]):
-        s = ''
-        for c in range(m.shape[0]):
-            s+= '#' if m[c, l] else '.'
-        print(s)
+    print('\n'.join([''.join(['#' if m[c, l] else '.' for c in range(m.shape[0])]) 
+        for l in range(m.shape[1])]))
     print()
+
 
 if __name__ == "__main__":
     fname = "day13-bs.txt";
@@ -104,6 +77,3 @@ if __name__ == "__main__":
     M, pliages = day13_load(fname)
     M = part2(M, pliages)
     pprint(M) # LRFJBJEH
-
-    
-
