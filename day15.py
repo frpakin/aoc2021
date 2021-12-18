022330fc5207ecc324f32fb5dafa89314466b91b
frpakin <fpakin@gmail.com>
2021-12-18 15:58:02 +0100

    Day15 solved

--- day15.py
+++ day15.py
@@ -1,7 +1,6 @@
 import numpy as np
-from numpy.core.fromnumeric import shape
-from numpy.lib.function_base import i0
-from tqdm import tqdm
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import dijkstra
 
 def day15_load(fname):
     with open(fname) as f:        
@@ -22,53 +21,43 @@ def adj_finder(m, l, c):
     return adj
 
 
-
 def part1(m):
-    costs = [ 0 for _ in range(m.shape[0])]
+    smat = [ [], [], [] ]
     for i in range(m.shape[0]):
-        costs[i] = np.sum(M, axis=0)[i]
-        for j in range(i):
-            costs[i] += m[0, j]
-        for j in range(i, m.shape[1]):
-            costs[i] += m[0, j]
-    GLOBAL_MIN = min(costs)
-    for i in range(m.shape[1]):
-        costs[i] = np.sum(M, axis=1)[i]
-        for j in range(i):
-            costs[i] += m[j, 0]
-        for j in range(i, m.shape[0]):
-            costs[i] += m[j, 0]
-    GLOBAL_MIN = min(GLOBAL_MIN, min(costs))
-    start=(0,0)
-    end = (m.shape[0]-1, m.shape[1]-1)
-    results = {}
-    next = [ [start] ]
-    costs = [ 0 ]
-    while len(next) != 0:
-        new = []
-        costs_new = []
-        for i in range(len(next)):
-            cost = costs[i]
-            if cost < GLOBAL_MIN:
-                path = next[i]
-                if path[-1] == end:
-                    results[cost] = path
-                    if cost < GLOBAL_MIN:
-                        print("Part 1 {:d} -> {:d}".format(GLOBAL_MIN, cost))
-                        GLOBAL_MIN = cost
-                else:
-                    for a in adj_finder(m, path[-1][0], path[-1][1]):
-                        if a not in path:
-                            new.append(path + [a])
-                            costs_new.append(cost + m[path[-1]])
-        next = new
-        costs = costs_new
-        print("       len: {:d} GLOBAL_MIN: {:d}".format(len(next), GLOBAL_MIN))
-    return GLOBAL_MIN
+        for j in range(m.shape[1]):            
+            k = i * m.shape[0] + j
+            for a in adj_finder(m, i, j):            
+                l = a[0] * m.shape[0] + a[1]
+                smat[0].append(k)
+                smat[1].append(l)
+                smat[2].append(m[a])
+                
+    graph = csr_matrix((smat[2], (smat[0], smat[1])), dtype=np.int8)
+    #print(graph)
+    dist_matrix = dijkstra(csgraph=graph, directed=True, indices=0, return_predecessors=False, min_only=True)
+    #print(dist_matrix)
+    return dist_matrix[-1]
 
 
 def part2(m):
-    ret = 0
+    full =  np.zeros((m.shape[0]*5, 5*m.shape[1]), dtype=np.int8)
+    for i in range(5):
+        for j in range(5):
+            if i == 0 and j == 0:
+                full[ 0:m.shape[0], 0:m.shape[1]  ] = m
+            elif i == 0:
+                full[ 0:m.shape[0], j*m.shape[1]:(j+1)*m.shape[1]  ] = full[ 0:m.shape[0], (j-1)*m.shape[1]:(j)*m.shape[1]  ] + 1
+                a = full[ 0:m.shape[0], j*m.shape[1]:(j+1)*m.shape[1]  ] > 9
+                full[ 0:m.shape[0], j*m.shape[1]:(j+1)*m.shape[1]  ] [a] = 1
+            elif j == 0:
+                full[ i*m.shape[0]:(i+1)*m.shape[0], 0:m.shape[1]  ] = full[ (i-1)*m.shape[0]:(i)*m.shape[0], 0:m.shape[1]  ] + 1
+                a = full[ i*m.shape[0]:(i+1)*m.shape[0], 0:m.shape[1]  ] > 9
+                full[ i*m.shape[0]:(i+1)*m.shape[0], 0:m.shape[1]  ] [a] = 1
+            else:
+                full[ i*m.shape[0]:(i+1)*m.shape[0], j*m.shape[1]:(j+1)*m.shape[1]  ] = full[ (i)*m.shape[0]:(i+1)*m.shape[0], (j-1)*m.shape[1]:(j)*m.shape[1]  ] + 1
+                a = full[ i*m.shape[0]:(i+1)*m.shape[0], j*m.shape[1]:(j+1)*m.shape[1]  ] > 9
+                full[ i*m.shape[0]:(i+1)*m.shape[0], j*m.shape[1]:(j+1)*m.shape[1]  ] [a] = 1
+    ret = part1(full)
     return ret
 
 
@@ -76,8 +65,17 @@ if __name__ == "__main__":
     fname = "day15-bs.txt";
     M = day15_load(fname)
     ret = part1(M)
-    print("Part 1 {:s} {:d} (40)".format(fname, ret))
+    print("Part 1 {:s} {:d} (40)".format(fname, int(ret)))
     fname = "day15-s.txt";
     M = day15_load(fname)
     ret = part1(M)
-    print("Part 1 {:s} {:d} (?)".format(fname, ret))
+    print("Part 1 {:s} {:d} (592)".format(fname, int(ret)))
+
+    fname = "day15-bs.txt";
+    M = day15_load(fname)
+    ret = part2(M)
+    print("Part 2 {:s} {:d} (315)".format(fname, int(ret)))
+    fname = "day15-s.txt";
+    M = day15_load(fname)
+    ret = part2(M)
+    print("Part 2 {:s} {:d} (2897)".format(fname, int(ret)))
