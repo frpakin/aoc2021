d868cc977fcb958a71b4cc3922185e72ca9bfdad
frpakin <fpakin@gmail.com>
2022-01-14 16:06:59 +0100

    Added graphviz

--- day23.py
+++ day23.py
@@ -8,31 +8,38 @@ def day23_load():
                         (1, 2, {'weight': 2}),
                         (1, 4, {'weight': 2}),
                         (2, 3, {'weight': 1}),
+                        (2, 4, {'weight': 2}),
                         (4, 5, {'weight': 2}),
                         (4, 7, {'weight': 2}),
                         (5, 6, {'weight': 1}),
+                        (5, 7, {'weight': 2}),
                         (7, 8, {'weight': 2}),
                         (7, 10, {'weight': 2}),
                         (8, 9, {'weight': 1}),
+                        (8, 10, {'weight': 2}),
                         (10, 11, {'weight': 2}),
                         (10, 13, {'weight': 2}),
+                        (11, 13, {'weight': 2}),
                         (11, 12, {'weight': 1}),
                         (13, 14, {'weight': 1}) ] )
-
-
     return G
 
 
-def day23_part1(G):
+def day23_show1(G):
     subax1 = plt.subplot(121)
     nx.draw(G, with_labels=True, font_weight='bold')
     subax2 = plt.subplot(122)
     nx.draw_shell(G, with_labels=True, font_weight='bold')
     plt.show()
+
+
+def day23_part1(G):
+    
     return 0
 
 
 if __name__ == "__main__":
     G = day23_load()
+    day23_show1(G)
     ret = day23_part1(G)
     print("Part 1  {:d} (???)".format(ret))
